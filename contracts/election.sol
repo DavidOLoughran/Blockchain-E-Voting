c38fa6617d5f7499dacea1f16beba3265b2b5f44
David <g00362038@gmit.ie>
2021-11-25 12:48:19 +0000

    Truffle connected to front-end using Metamask/Web3

--- contracts/election.sol
+++ contracts/election.sol
@@ -2,5 +2,46 @@ pragma solidity ^0.5.0;
 
 contract Election {
     
-}
+    //Vote[] public votes;
+    
+    uint256 public cand1Count = 0;
+    uint256 public cand2Count = 0;
+    uint256 public voteCount = 0;
 
+
+
+
+    
+    struct Vote {
+        uint voteID; 
+        bool hasVoted;
+        string candidate; 
+        
+    }
+
+    mapping(uint => Vote) public votes;
+
+    constructor() public {
+        createVote("Candidate 1");
+    }
+
+    function createVote(string memory _candidate) public{
+
+        voteCount ++;
+        votes[voteCount] = Vote(voteCount, true , _candidate);
+    }
+    
+    // function addVote (uint userID, uint voteID, bool hasVoted, uint candidate) public{
+    //     votes.push(Vote(userID, voteID, hasVoted, candidate));
+    // }
+    
+    // function removeVote (uint voteID) public{
+        
+    //     for (uint i=0; i<votes.length; i++) {
+    //         if (voteID == votes[i].voteID) {
+    //             delete votes[i];
+   
+    //     }}
+    // }
+    
+}
